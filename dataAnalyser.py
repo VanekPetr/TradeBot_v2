80d74ce059f211ab56136a5cc369f913448c4058
Petr Vaněk <petrvanek@Petrs-MacBook-Pro.local>
2021-04-24 15:43:13 +0200

    TradeBot Class is created

--- dataAnalyser.py
+++ dataAnalyser.py
@@ -8,11 +8,10 @@ Created on Wed Nov 11 17:59:42 2020
 
 import pandas as pd
 import numpy as np
-import math
 
 
 # Function computing the geometric mean of annual returns 
-#----------------------------------------------------------------------     
+# ----------------------------------------------------------------------
 def meanRetAn(data):             
     Result = 1
     
@@ -22,93 +21,11 @@ def meanRetAn(data):
     Result = Result**(1/float(len(data.index)/52))-1
      
     return(Result)
-    
-"""
-    ----------------------------------------------------------------------
-    Data Analytics and Visualisation: ANALYSE THE DATA AND GET WEEKLY RETURNS  
-    ---------------------------------------------------------------------- 
-"""  
-def analyseData(data, startDate, endDate):
-   
-    # Modify the pandas table (get returns for a given time period)
-    #---------------------------------------------------------------------- 
-    
-    # DEFINE IF WE WORK WITH ISIN CODES OR NAMES OF MUTUAL FUNDS
-    workPrices = data
-    
-    # MODIFY THE DATA
-    pricesWed = workPrices[workPrices.index.weekday==2]     ## 1, Only wednesdays
-
-    startdate = startDate                                   ## 2, Subset of dates                               
-    enddate = endDate
-    
-    pricesSub = pricesWed[pricesWed.index < enddate]        #define time interval
-    pricesSub = pricesSub[pricesSub.index > startdate]
-    
-    weeklyPrices = pricesSub.dropna(axis=1)                 #delete NaN columns
-    
-    # GET WEEKLY RETURNS
-    weeklyReturns = weeklyPrices.pct_change()                  
-    weeklyReturns = weeklyReturns.drop(weeklyReturns.index[:1])     #drop first NaN row
-    
-    
-    # GET MONTHLY RETURNS
-    numMonths = math.floor((len(weeklyPrices.index)-1)/4)      
-    monthlyPrices = pd.DataFrame(columns=weeklyPrices.columns)
-    for i in range(numMonths+1):
-        oneRow = weeklyPrices.iloc[0+4*i,:]
-        monthlyPrices = monthlyPrices.append(oneRow)
-        
-    monthlyReturns = monthlyPrices.pct_change()
-    monthlyReturns = monthlyReturns.drop(monthlyReturns.index[:1])
-    
-    """
-    # 5, Get Annual returns
-    N_results = len(monthlyPrices.index)-12
-    annualReturns = pd.DataFrame(columns=monthlyPrices.columns)
-    for i in range(N_results):
-        annualReturns = annualReturns.append((monthlyPrices.iloc[i+12,:]
-        - monthlyPrices.iloc[i,:])/monthlyPrices.iloc[i,:], ignore_index=True)
-    
-    # 5, Get Annual returns
-    N_results = int(np.floor((len(monthlyPrices.index)-1)/12))
-    zbyt = (len(monthlyPrices.index)-1)%12
-    annualReturns = pd.DataFrame(columns=monthlyPrices.columns)   
-    for i in range(N_results):
-        annualReturns = annualReturns.append((monthlyPrices.iloc[zbyt + 12 + 12*i,:]
-        - monthlyPrices.iloc[zbyt + i*12,:])/monthlyPrices.iloc[zbyt + i*12,:],
-            ignore_index=True)
-    """
-    
-    # Analysis of the data
-    #---------------------------------------------------------------------- 
-    
-    # TABLE WITH AVG RET AND STD OF RET
-    mu_ga = meanRetAn(weeklyReturns)                   #anual geometric mean
-    stdev_a = weeklyReturns.std(axis=0) * np.sqrt(52)   #standard deviation of Annual Returns
-    #stdev_a = annualReturns.std(axis=0)        
-
-    statDf = pd.concat([mu_ga,stdev_a], axis=1)         #table
-    statName = ["Average Annual Returns","Standard Deviation of Returns"]
-    statDf.columns = statName                           #add names
-    
-    # COMPUTE SHARPE RATIO AND ADD IT INTO THE TABLE
-    sharpe = statDf.loc[:,"Average Annual Returns"]/statDf.loc[:,"Standard Deviation of Returns"]
-    statDf = pd.concat([statDf,sharpe], axis=1)         #add sharpe ratio into the table
-
-    statName = ["Average Annual Returns","Standard Deviation of Returns",
-                "Sharpe Ratio"]
-    statDf.columns = statName 
-    
-    # ADD NAMES INTO THE TABLE
-    statDf["Name"] = statDf.index
-    
-    return(statDf, weeklyReturns)
 
 """
-    ----------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------
     Machine Learning and Advanced Statistical Methods: GET STATISTICS BASED ON WEEKLY RETURNS 
-    ---------------------------------------------------------------------- 
+    ----------------------------------------------------------------------------------------- 
 """      
 def getStat(data):
     # TABLE WITH AVG RET AND STD OF RET
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Created on Wed Nov 11 15:50:42 2020
 """
 import numpy as np
 
-from dataAnalyser import analyseData, getStat, finalStat
+from dataAnalyser import meanRetAn, getStat, finalStat
 from dataGraph import plotInteractive, plotOptimization
 from MST import MinimumSpanningTree
 from Clustering import Cluster, pickCluster
@@ -14,71 +14,110 @@ from CVaRtargets import targetsCVaR
 from CVaRmodel import modelCVaR
 
 from pandas_datareader import data
+import pandas as pd
+import plotly.express as px
 
 
-# WHAT TIME PERIOD DO WE WANT WORK WITH?
-#------------------------------------------------------------------
-startDate = "2015-09-23"
-endDate = "2019-09-22"
-#thesis dates:  startDate ="2016-07-25" & endDate = "2020-04-30"
+class TradeBot(object):
+    """
+    Python class analysing financial products and based on machine learning algorithms and mathematical
+    optimization suggesting optimal portfolio of assets.
+    """
 
-# TICKERS OF ETFs WE ARE GOING TO WORK WITH
-#------------------------------------------------------------------
+    def __init__(self, start, end, assets):
+        # DOWNLOAD THE ADJUSTED DAILY PRICES FROM YAHOO DATABASE
+        dailyPrices = data.DataReader(assets, 'yahoo', start, end)["Adj Close"]
 
-tickers = ["ANGL","ASHR","BIV","BKLN","BNDX","BOND","BRF","CGW","CMBS",
-           "CMF","CORP","CSM","CWB","DBA","DBB","DBO","DBS","DBV","DES","DGL",
-           "DGRW","DIA","DLS","DOL","DON","DSI","DXGE","DXJ","EBND","ECH","EDEN",
-           "EEM","EFV","EIDO","EIRL","ENZL","EPHE","EWD","EWG","EWH","EWI","EWL",
-           "EWM","EWN","EWQ","EWS","EWT","EWU","EWW","EWY","EZA","FBT","FCG",
-           "FCOM","FDD","FDL","FEU","FEX","FLRN","FPX","FTA","FTCS","FTSM","FXA",
-           "FXB","FXC","FXE","FXF","FXR","FXY","FXZ","GBF","GNMA","GREK","GSY",
-           "HDV","HEDJ","HEFA","HYD","HYEM","IAU","IBND","IDLV","IGE","IGN","IGV",
-           "IHI","INDA","IOO","IPE","IPFF","IQDF","ISTB","ITA","ITB","ITM","IUSV",
-           "IVOO","IVOV","IWC","IWN","IWO","IWY","IXG","IXN","IYE","IYY","IYZ",
-           "JKD","JKE","JKG","JPXN","KBWP","KOL","KRE","KXI","LTPZ","MCHI","MDYG",
-           "MDYV","MGV","MLPA","MOAT","MOO","MTUM","OIH","PALL","PCY","PDP","PEY",
-           "PFXF","PHB","PHO","PJP","PKW","PRFZ","PSCC","PSCT","PSCU","PSK","PSL",
-           "PUI","PWB","PWV","PWZ","QDF","QUAL","RDIV","REM","REZ","RFG","RING",
-           "RSX","RTH","RWJ","RWL","RWX","RXI","RYF","SCHC","SCHE","SCJ","SDIV",
-           "SDOG","SGDM","SGOL","SHM","SILJ","SIZE","SLQD","SLY","SLYG","SMLV",
-           "SNLN","SOCL","SPHQ","SPYG","TAN","TDIV","TDTT","THD","TIP","TOK","TUR",
-           "UGA","URA","URTH","USDU","VBK","VCLT","VEA","VLUE","VNM","VOE","VONE",
-           "VONG","VONV","VOT","VXF","XBI","XES","XHS","XLE","XLG","XLI","XLK",
-           "XLP","XLU","XLV","XLY","XME","XPH","XRT","XSD","XTN","ZROZ"]
+        # GET WEEKLY RETURNS
+        # Get prices only for Wednesdays and delete Nan columns
+        pricesWed = dailyPrices[dailyPrices.index.weekday == 2].dropna(axis=1)
+        # Get weekly returns
+        self.weeklyReturns = pricesWed.pct_change().drop(pricesWed.index[0])  # drop first NaN row
 
-#tickers = ['BTC-USD']
-"""
-    ----------------------------------------------------------------------
-    DATA ANALYTICS AND VISUALISATION 
-    ----------------------------------------------------------------------
-"""
-# DOWNLOAD THE DATA FROM YAHOO DATABASE
-#------------------------------------------------------------------
-dailyPrices = data.DataReader(tickers, 'yahoo', startDate, endDate)
-dailyPrices = dailyPrices["Adj Close"]
-    
+    # METHOD COMPUTING ANNUAL RETURNS, ANNUAL STD. DEV. & SHARPE RATIO OF ASSETS
+    def __get_stat(self, start, end):
 
-# ANALYSE THE DATA AND GET WEEKLY RETURNS  
-#------------------------------------------------------------------
-dataStat, weeklyReturns = analyseData(data = dailyPrices,
-                                      startDate = startDate,
-                                      endDate = endDate) 
+        # ANALYZE THE DATA for a given time period
+        weeklyData = self.weeklyReturns[(self.weeklyReturns.index > start) & (self.weeklyReturns.index < end)].copy()
+
+        # Create table with summary statistics
+        mu_ga = meanRetAn(weeklyData)                   # Annualised geometric mean of returns
+        stdev_a = weeklyData.std(axis=0) * np.sqrt(52)  # Annualised standard deviation of returns
+        sharpe = round(mu_ga/stdev_a, 2)                # Sharpe ratio of each financial product
+
+        # Write all results into a data frame
+        statDf = pd.concat([mu_ga, stdev_a, sharpe], axis=1)
+        statDf.columns = ["Average Annual Returns", "Standard Deviation of Returns", "Sharpe Ratio"]
+        statDf["Name"] = statDf.index                   # Add names into the table
+
+        return statDf
+
+    # METHOD TO PLOT THE OVERVIEW OF THE FINANCIAL PRODUCTS IN TERMS OF RISK AND RETURNS
+    def plot_dots(self, start, end, ML, MLsubset):
+        # Get statistics for a given time period
+        data = self.__get_stat(start, end)
+
+        # IF WE WANT TO HIGHLIGHT THE SUBSET OF ASSETS BASED ON ML
+        if ML == "MST":
+            setColor = "Type"
+            data.loc[:, "Type"] = "The rest of assets"
+            for fund in MLsubset:
+                data.loc[fund, "Type"] = "Subset based on MST"
+        if ML == "Clustering":
+            setColor = "Type"
+            data.loc[:, "Type"] = MLsubset.loc[:, "Cluster"]
+        if ML == None:
+            setColor = None
+
+        # PLOTTING Data
+        fig = px.scatter(data,
+                         x="Standard Deviation of Returns",
+                         y="Average Annual Returns",
+                         hover_data=["Sharpe Ratio", "Name"],
+                         color=setColor,
+                         title="The Relationship between Annual Returns and Standard Deviation of Returns from "
+                               + start + " to " + end)
+
+        # AXIS IN PERCENTAGES
+        fig.layout.yaxis.tickformat = ',.1%'
+        fig.layout.xaxis.tickformat = ',.1%'
+
+        fig.show()
 
 
-# PLOT INTERACTIVE GRAPH
-#------------------------------------------------------------------
-plotInteractive(data = dataStat,
-                start = startDate,
-                end = endDate,
-                ML = None, MLsubset = None)
+if __name__ == "__main__":
 
+    # TICKERS OF ETFs WE ARE GOING TO WORK WITH
+    tickers = ["ANGL","ASHR","BIV","BKLN","BNDX","BOND","BRF","CGW","CMBS",
+               "CMF","CORP","CSM","CWB","DBA","DBB","DBO","DBS","DBV","DES","DGL",
+               "DGRW","DIA","DLS","DOL","DON","DSI","DXGE","DXJ","EBND","ECH","EDEN",
+               "EEM","EFV","EIDO","EIRL","ENZL","EPHE","EWD","EWG","EWH","EWI","EWL",
+               "EWM","EWN","EWQ","EWS","EWT","EWU","EWW","EWY","EZA","FBT","FCG",
+               "FCOM","FDD","FDL","FEU","FEX","FLRN","FPX","FTA","FTCS","FTSM","FXA",
+               "FXB","FXC","FXE","FXF","FXR","FXY","FXZ","GBF","GNMA","GREK","GSY",
+               "HDV","HEDJ","HEFA","HYD","HYEM","IAU","IBND","IDLV","IGE","IGN","IGV",
+               "IHI","INDA","IOO","IPE","IPFF","IQDF","ISTB","ITA","ITB","ITM","IUSV",
+               "IVOO","IVOV","IWC","IWN","IWO","IWY","IXG","IXN","IYE","IYY","IYZ",
+               "JKD","JKE","JKG","JPXN","KBWP","KOL","KRE","KXI","LTPZ","MCHI","MDYG",
+               "MDYV","MGV","MLPA","MOAT","MOO","MTUM","OIH","PALL","PCY","PDP","PEY",
+               "PFXF","PHB","PHO","PJP","PKW","PRFZ","PSCC","PSCT","PSCU","PSK","PSL",
+               "PUI","PWB","PWV","PWZ","QDF","QUAL","RDIV","REM","REZ","RFG","RING",
+               "RSX","RTH","RWJ","RWL","RWX","RXI","RYF","SCHC","SCHE","SCJ","SDIV",
+               "SDOG","SGDM","SGOL","SHM","SILJ","SIZE","SLQD","SLY","SLYG","SMLV",
+               "SNLN","SOCL","SPHQ","SPYG","TAN","TDIV","TDTT","THD","TIP","TOK","TUR",
+               "UGA","URA","URTH","USDU","VBK","VCLT","VEA","VLUE","VNM","VOE","VONE",
+               "VONG","VONV","VOT","VXF","XBI","XES","XHS","XLE","XLG","XLI","XLK",
+               "XLP","XLU","XLV","XLY","XME","XPH","XRT","XSD","XTN","ZROZ"]
 
+    # INITIALIZATION OF THE CLASS
+    algo = TradeBot(start="2015-09-23", end="2019-09-22", assets=tickers)
 
-"""
-    ----------------------------------------------------------------------
-    MACHINE LEARNING AND ADVANCED STATISTICAL METHODS
-    ----------------------------------------------------------------------
-""" 
+    # PLOT INTERACTIVE GRAPH
+    algo.plot_dots(start="2016-01-01", end="2018-01-01", ML=None, MLsubset=None)
+
+'''
+### MACHINE LEARNING
+
 # DIVIDE DATASET INTO TRAINING AND TESTING PART?
 #------------------------------------------------------------------
 divide = True
@@ -88,40 +127,40 @@ if divide != False:
     # ONE HALF OF THE DATA, BREAKPOINT IN TRAINING AND TESTING DATASET
     breakPoint = int(np.floor(len(weeklyReturns.index)/2))
     # DEFINITION OF TRAINING AND TESTING DATASETS
-    trainDataset = weeklyReturns.iloc[0:breakPoint,:] 
-    testDataset = weeklyReturns.iloc[breakPoint:,:] 
-    
+    trainDataset = weeklyReturns.iloc[0:breakPoint,:]
+    testDataset = weeklyReturns.iloc[breakPoint:,:]
+
     dataPlot = getStat(data = trainDataset)
     endTrainDate = str(trainDataset.index.date[-1])
     startTestDate = str(testDataset.index.date[0])
     lenTest = len(testDataset.index)
-else: 
+else:
     trainDataset = weeklyReturns
     dataPlot = dataStat
     lenTest = 0
-    
+
 
 # RUN THE MINIMUM SPANNING TREE METHOD
 #------------------------------------------------------------------
-nMST = 3                        # Select how many times run the MST method   
+nMST = 3                        # Select how many times run the MST method
 subsetMST_df = trainDataset
 for i in range(nMST):
     subsetMST, subsetMST_df, corrMST_avg, PDI_MST = MinimumSpanningTree(subsetMST_df)
-    
+
 # PLOT
 plotInteractive(data = dataPlot,
-                ML = "MST", 
+                ML = "MST",
                 MLsubset = subsetMST,
                 start = startDate,
                 end = endTrainDate)
-    
- 
-# RUN THE CLUSTERING METHOD 
+
+
+# RUN THE CLUSTERING METHOD
 #------------------------------------------------------------------
 clusters = Cluster(trainDataset,
                    nClusters = 3,
                    dendogram = False)
-    
+
 # SELECT ASSETS
 subsetCLUST, subsetCLUST_df = pickCluster(data = trainDataset,
                                           stat = dataPlot,
@@ -130,52 +169,45 @@ subsetCLUST, subsetCLUST_df = pickCluster(data = trainDataset,
 
 # PLOT
 plotInteractive(data = dataPlot,
-                ML = "Clustering", 
+                ML = "Clustering",
                 MLsubset = clusters,
                 start = startDate,
                 end = endTrainDate)
-    
-    
 
-"""
-    ----------------------------------------------------------------------
-    SCENARIO GENERATION
-    ----------------------------------------------------------------------
-""" 
 
-# THE BOOTSTRAPPING SCENARIO GENERATION 
+
+### SCENERIO GENERATION
+
+# THE BOOTSTRAPPING SCENARIO GENERATION
 #------------------------------------------------------------------
 # FOR THE MST METHOD
 BOOT_sim_MST = BOOT(data = weeklyReturns[subsetMST],    #subsetMST or subsetCLUST
                 nSim = 250,                             #number of scenarios per period
-                N_test = lenTest) 
+                N_test = lenTest)
 
 
 # FOR THE CLUSTERING METHOD
 BOOT_sim_CLUST = BOOT(data = weeklyReturns[subsetCLUST],#subsetMST or subsetCLUST
                       nSim = 250,
-                      N_test = lenTest) 
+                      N_test = lenTest)
 
-# THE MONTE CARLO SCENARIO GENERATION 
+# THE MONTE CARLO SCENARIO GENERATION
 #------------------------------------------------------------------
 # FOR THE MST METHOD
 MC_sim_MST = MC(data = subsetMST_df,                    #subsetMST_df or subsetCLUST_df
                 nSim = 250,
-                N_test = lenTest)    
+                N_test = lenTest)
 
 
 
-# FOR THE CLUSTERING METHOD  
+# FOR THE CLUSTERING METHOD
 MC_sim_CLUST = MC(data = subsetCLUST_df,                #subsetMST_df or subsetCLUST_df
                   nSim = 250,
-                  N_test = lenTest)  
+                  N_test = lenTest)
 
 
-"""
-    ----------------------------------------------------------------------
-    MATHEMATICAL OPTIMIZATION
-    ----------------------------------------------------------------------
-""" 
+### MATHEMATICAL OPTIMIZATION
+
 # TARGETS GENERATION
 #------------------------------------------------------------------
 targets, benchmarkPortVal = targetsCVaR(start_date = startDate,
@@ -184,7 +216,7 @@ targets, benchmarkPortVal = targetsCVaR(start_date = startDate,
                                         benchmark = ["URTH"],   #MSCI World benchmark
                                         test_index = testDataset.index.date,
                                         budget = 100,
-                                        cvar_alpha=0.05) 
+                                        cvar_alpha=0.05)
 
 
 # MATHEMATICAL MODELING
@@ -208,5 +240,7 @@ plotOptimization(performance = portValue.copy(),
 
 # STATISTICS
 #------------------------------------------------------------------
-finalStat(portValue) 
+finalStat(portValue)
 finalStat(benchmarkPortVal)
+
+'''
